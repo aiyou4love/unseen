f9c90271cc6ec4a415e846567bc30595b8446f46
zyouhua <zyouhua@gmail.com>
2016-04-18 22:24:52 +0800

    u

--- common/utility/StringMethod.hpp
+++ common/utility/StringMethod.hpp
@@ -2,22 +2,22 @@
 
 namespace std {
 	
-	template<typename __t1, typename __t2>
-	void stringSplit(const char * nValue, __t1& nResult, const char * nSplit)
+	template<typename T1, typename T2>
+	void stringSplit(const char * nValue, T1& nResult, const char * nSplit)
 	{
 		size_t size_ = strlen(nValue);
 		size_t start_ = 0, end_ = 0;
 		while (start_ < size_) {
 			end_ = strcspn(nValue + start_, nSplit);
 			string value_(nValue + start_, end_);
-			__t2 t_ = __convert<const char *, __t2>(value_.c_str());
+			T2 t_ = __convert<const char *, T2>(value_.c_str());
 			nResult.push_back(t_);
 			start_ += (end_ + 1);
 		}
 	}
 	
-	template<typename __t1>
-	void stringCrcSplit(const char * nValue, __t1& nResult, const char * nSplit)
+	template<typename T>
+	void stringCrcSplit(const char * nValue, T& nResult, const char * nSplit)
 	{
 		size_t size_ = strlen(mValue);
 		size_t start_ = 0, end_ = 0;
@@ -30,19 +30,19 @@ namespace std {
 		}
 	}
 	
-	template<typename __t1, typename __t2>
-	string stringCombine(__t1& nValue, const char * nCombine)
+	template<typename T1, typename T2>
+	string stringCombine(T1& nValue, const char * nCombine)
 	{
 		bool first_ = true; string result_("");
 		auto it = nValue.begin();
 		for ( ; it != nValue.end(); ++it ) {
-			__t2 value_ = (*it);
+			T2 value_ = (*it);
 			if ( first_ ) {
-				result_ += __convert<__t2, string>(value_);
+				result_ += __convert<T2, string>(value_);
 				first_ = false;
 			} else {
 				result_ += nCombine;
-				result_ += __convert<__t2, string>(value_);
+				result_ += __convert<T2, string>(value_);
 			}
 		}
 		return result_;
